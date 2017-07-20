c290bd88756dd95f57013a62a97fda04e473c8a4
myko-alpha57 <nhsystems@Casper-Ravn-Srensens-iMac.local>
2017-07-20 11:09:13 +0200

    Language learning

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function(){
+
+  // when get started button is clicked
+  $('.btn-get-started').click(function(){
+    $('.intro-heading').addClass('hidecontent');
+    $('.want-to-learn').removeClass('hidecontent').addClass('showcontent');
+
+  });
+  
+});
+
+
+
+
+
+
